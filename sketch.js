d71d4fe41b86bd752b138b0f522c9a9b95d639cd
AditiSingh629 <64412908+AditiSingh629@users.noreply.github.com>
2020-05-21 20:41:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var topy,t2,t3,t4
+function setup() {
+  createCanvas(800,400);
+
+}
+
+function draw() {
+  background(255,255,255); 
+ topy = triangle(230, 155, 440, 100, 526, 155) 
+ t2 = rect(230,160,290,240)  
+  drawSprites();
+}
